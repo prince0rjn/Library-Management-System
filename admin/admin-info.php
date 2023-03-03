c7656d85f3f73a38e955f4e4b071730c6ff13a49
Prince Ranjan <prince0rjn@gmail.com>
2023-03-03 10:33:40 +0530

    First Commit

---
+++ admin/admin-info.php
@@ -0,0 +1,141 @@
+<?php 
+session_start();
+include('includes/config.php');
+error_reporting(0);
+if(strlen($_SESSION['alogin'])==0)
+    {   
+header('location:index.php');
+}
+else{ 
+if(isset($_POST['update']))
+{    
+$username=$_SESSION['alogin'];  
+$fname=$_POST['fullname'];
+$email=$_POST['email'];
+
+$sql="update admin set FullName=:fname, AdminEmail=:email where UserName=:username";
+// $sql="UPDATE `admin` SET `FullName` = $fname, `UserName` = $uname, `AdminEmail` = $email WHERE `admin`.`username` = $username;"
+$query = $dbh->prepare($sql);
+$query->bindParam(':username',$username,PDO::PARAM_STR);
+$query->bindParam(':fname',$fname,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+
+
+$query->execute();
+
+echo '<script>alert("Your profile has been updated")</script>';
+}
+
+?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <!--[if IE]>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <![endif]-->
+    <title>Online Library Management System | Student Signup</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' /> 
+
+</head>
+<body>
+    <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+    <div class="content-wrapper">
+         <div class="container">
+        <div class="row pad-botm">
+            <div class="col-md-12">
+                <h4 class="header-line">My Profile</h4>
+                
+                            </div>
+
+        </div>
+             <div class="row">
+           
+<div class="col-md-9 col-md-offset-1">
+               <div class="panel panel-danger">
+                        <div class="panel-heading">
+                           My Profile
+                        </div>
+                        <div class="panel-body">
+                            <form name="signup" method="post">
+<?php 
+$username=$_SESSION['alogin'];
+
+$sql="SELECT FullName,AdminEmail,UserName,updationDate from  admin  where UserName=:username";
+$query = $dbh -> prepare($sql);
+$query-> bindParam(':username', $username, PDO::PARAM_STR);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{               ?>  
+
+<div class="form-group">
+<label>Username : </label>
+<?php echo htmlentities($result->UserName);?>
+</div>
+<div class="form-group">
+<label>Name : </label>
+<?php echo htmlentities($result->FullName);?>
+</div>
+
+<div class="form-group">
+<label>Email : </label>
+<?php echo htmlentities($result->AdminEmail);?>
+</div>
+<?php if($result->UpdationDate!=""){?>
+<div class="form-group">
+<label>Last Updation Date : </label>
+<?php echo htmlentities($result->updationDate);?>
+</div>
+<?php } ?>
+
+
+
+<div class="form-group">
+<label>Enter Full Name</label>
+<input class="form-control" type="text" name="fullname" value="<?php echo htmlentities($result->FullName);?>" autocomplete="off" required />
+</div>
+
+
+                                        
+<div class="form-group">
+<label>Enter Email</label>
+<input class="form-control" type="email" name="email" id="emailid" value="<?php echo htmlentities($result->AdminEmail);?>"  autocomplete="off" required />
+</div>
+<?php }} ?>
+                              
+<button type="submit" name="update" class="btn btn-primary" id="submit">Update Now </button>
+
+                                    </form>
+                            </div>
+                        </div>
+                            </div>
+        </div>
+    </div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+    <?php include('includes/footer.php');?>
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
+<?php } ?>
